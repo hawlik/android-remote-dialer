3a6007d4e07e26fec1507cdc9e96251ebd5be672
hawlik <thawliczek@gmail.com>
2026-07-22 15:19:58 +0200

    chore: release 0.3

--- phone/build.gradle.kts
+++ phone/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.remotedialer.phone"
         minSdk = 34
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
     }
 
     buildTypes {
